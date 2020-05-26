4ec54be2d3f629de8c9da0e7c0b87ab8460ef34f
NamWoojin <47585253+NamWoojin@users.noreply.github.com>
2020-05-26 13:11:58 +0900

    레이아웃 수정

--- js/detail-control.js
+++ js/detail-control.js
@@ -1,11 +1,11 @@
 var pageNum = 0;
 
 
-function sendPageNum(htmlAddress,week){
+function sendPageNum(htmlAddress,week){ //이동하려는 html뒤에 식별할 수 있는 표시 추가
   location.href=htmlAddress+"?page:"+week;
 }
 
-function getPageNum(){
+function getPageNum(){ //이동한 html주소에서 표시 읽어오기
     temp = location.href.split("?");
     data=temp[1].split(":");
     pageNum = data[1];
@@ -13,7 +13,7 @@ function getPageNum(){
     return pageNum;
 }
 
-function setDisplay(){
+function setDisplay(){ //display제어
   var pageNum = getPageNum();
   var classes = ['week1','week2'];
 
